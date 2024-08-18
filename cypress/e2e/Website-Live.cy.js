43a2210152916a5ca89cab59301a53ca93ad2c16
DiONJERRY <dionjerry442@gmail.com>
2024-08-18 14:32:06 -0500

    --Added resources file and key

--- cypress/e2e/Website-Live.cy.js
+++ cypress/e2e/Website-Live.cy.js
@@ -1,5 +1,19 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://oasis-staging.nomadinternet.com')
-  })
-})
+
+describe('Website Address Check', () => {
+  let resources;
+
+  before(() => {
+    cy.fixture('resources').then((res) => {
+      resources = res;
+      // this is for setting the base url in test this is accessed from the resource file in fixture folder
+    });
+  });
+
+  it('Check Address', () => {
+    cy.visit(resources.baseUrl);
+  });
+
+  it('Checks title', () => {
+    
+  });
+});
